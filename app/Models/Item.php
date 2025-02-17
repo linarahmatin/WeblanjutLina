c1d9cba3abd653be5976a7784c12b0532726e1f1
linarahmatin <linarahmatin9@gmail.com>
2025-02-17 15:39:17 +0700

    Fix item

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace App\Models;
+namespace App\Models; // // Menentukan namespace untuk model ini agar sesuai dengan struktur Laravel
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Menggunakan trait HasFactory untuk mendukung pembuatan factory
+use Illuminate\Database\Eloquent\Model; // Menggunakan class Model sebagai dasar dari model ini
 
-class Item extends Model
+class Item extends Model // Mendefinisikan kelas Item yang merupakan turunan dari Model Laravel
 {
-    use HasFactory;
+    use HasFactory; // Menggunakan trait HasFactory untuk memungkinkan pembuatan data dummy menggunakan factory
+
+    protected $fillable = [ // Menentukan atribut yang boleh diisi secara massal (mass assignment)
+        'name', // Nama item
+        'description', // Deskripsi item
+    ];
 }
