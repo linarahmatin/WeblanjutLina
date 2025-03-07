c4ac730f002252b9f35be15135df22bea6453f3a
linarahmatin <linarahmatin9@gmail.com>
2025-03-07 14:33:00 +0700

    js4 2.4

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -4,14 +4,20 @@
     <title>Data User</title>
 </head>
 <body> 
-    <h1>Data User</h1> 
-    <table border="1" cellpadding="2" cellspacing="0"> 
-        <tr> 
-            <th>Jumlah Pengguna</th> 
-        </tr> 
-        <tr> 
-            <td>{{ $jumlahPengguna }}</td> 
-        </tr> 
-    </table> 
+<h1>Data User</h1> 
+        <table border="1" cellpadding="2" cellspacing="0"> 
+            <tr> 
+                <th>ID</th> 
+                <th>Username</th> 
+                <th>Nama</th>
+                <th>ID Level Pengguna</th> 
+            </tr> 
+            <tr> 
+                <td>{{ $data->user_id }}</td> 
+                <td>{{ $data->username }}</td> 
+                <td>{{ $data->nama }}</td> 
+                <td>{{ $data->level_id }}</td>
+            </tr> 
+        </table> 
 </body>
 </html>
