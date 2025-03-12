aa527a7c7bb07992afc49ff98abdc21f0049b703
linarahmatin <linarahmatin9@gmail.com>
2025-03-12 14:34:08 +0700

    js 5 praktikum 3

--- PWL_POS/app/Http/Controllers/KategoriController.php
+++ PWL_POS/app/Http/Controllers/KategoriController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\KategoriModel;
+use Illuminate\Http\Request;
 use App\DataTables\KategoriDataTable;
+
 
 // use Illuminate\support\facades\DB;
 
@@ -34,6 +37,20 @@ class KategoriController extends Controller
     public function index(KategoriDataTable $dataTable)
 {
     return $dataTable->render('kategori.index');
+    }
+    public function create()
+{
+    return view('kategori.create');
+}
+
+    public function store(Request $request)
+{
+    KategoriModel::create([
+        'kategori_kode' => $request->kodeKategori,
+        'kategori_nama' => $request->namaKategori,
+    ]);
+
+    return redirect('/kategori');
     }
 }
 
