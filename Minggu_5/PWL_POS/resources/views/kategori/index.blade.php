46863da20534d37aa94b5be89be9ecc03d889edf
linarahmatin <linarahmatin9@gmail.com>
2025-03-14 12:25:45 +0700

    Tugas Praktikum No.1

--- Minggu_5/PWL_POS/resources/views/kategori/index.blade.php
+++ Minggu_5/PWL_POS/resources/views/kategori/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 {{-- Customize layout sections --}}
-
 @section('subtitle', 'Kategori')
 @section('content_header_title', 'Home')
 @section('content_header_subtitle', 'Kategori')
@@ -9,15 +8,17 @@
 @section('content')
     <div class="container">
         <div class="card">
-            <div class="card-header">Manage Kategori</div>
+        <div class="card-header d-flex justify-content-between align-items-center">
+            <span>Manage Kategori</span>
+            <a href="{{ url('kategori/create') }}" class="btn btn-primary ms-auto">Add</a>
+        </div>
             <div class="card-body">
-                {{ $dataTable->table() }}
+                {!! $dataTable->table() !!}
             </div>
         </div>
     </div>
-
 @endsection
 
 @push('scripts')
-    {{ $dataTable->scripts() }}
+    {!! $dataTable->scripts() !!}
 @endpush
