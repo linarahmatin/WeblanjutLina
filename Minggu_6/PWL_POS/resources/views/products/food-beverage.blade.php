cba294820edb45a664ff5582852c7c55c0d96efc
linarahmatin <linarahmatin9@gmail.com>
2025-03-18 12:34:01 +0700

    aploud project

---
+++ Minggu_6/PWL_POS/resources/views/products/food-beverage.blade.php
@@ -0,0 +1,6 @@
+<html>
+<head><title>food beverage</title></head>
+<body>
+    <h1>Food & Beverage Products</h1>
+</body>
+</html>
---
+++ Minggu_6/PWL_POS/routes/web.php
@@ -0,0 +1,69 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
+use App\Http\Controllers\KategoriController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route Home
+// Route::get('/', [HomeController::class, 'index']);
+
+// Route Products (dengan prefix "category")
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [ProductController::class, 'beautyHealth']);
+    Route::get('/home-care', [ProductController::class, 'homeCare']);
+    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+});
+
+// Route User (menggunakan parameter)
+Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
+
+// Route Penjualan (POS)
+Route::get('/sales', [SalesController::class, 'index']);
+
+Route::get('/level', [LevelController::class, 'index']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/user', [UserController::class, 'index']);
+
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+
+Route::get('user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::get('/kategori', [KategoriController::class, 'index']);
+
+Route::get('/kategori/create', [KategoriController::class, 'create']);
+
+Route::post('/kategori', [KategoriController::class, 'store']);
+
+Route::get('/kategori/edit/{id}', [KategoriController::class, 'edit'])->name('kategori.edit');
+Route::put('/kategori/{id}', [KategoriController::class, 'update'])->name('kategori.update');
+
+Route::post('/kategori/delete/{id}', [KategoriController::class, 'destroy']);
