de2c5fc20c733e64f8f4244c4bf0da6fcada28d3
linarahmatin <linarahmatin9@gmail.com>
2025-03-22 14:59:57 +0700

    praktikum 4 js 5.2

--- Minggu_5.2/PWL_POS/app/Http/Controllers/UserController.php
+++ Minggu_5.2/PWL_POS/app/Http/Controllers/UserController.php
@@ -254,9 +254,12 @@ class UserController extends Controller
 
         $activeMenu = 'user'; // set menu yang sedang aktif
 
+        $level = LevelModel::all();  
+
         return view('user.index', [
             'breadcrumb' => $breadcrumb,
             'page'       => $page,
+            'level'      => $level,
             'activeMenu' => $activeMenu
         ]);
     }
@@ -266,6 +269,11 @@ class UserController extends Controller
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
+
+        // filter data user berdasarkan level_id
+        if ($request->level_id) {
+            $users ->where('level_id', $request->level_id);
+        }
 
         return DataTables::of($users)
             ->addIndexColumn() // Menambahkan kolom index / nomor urut (default: DT_RowIndex)
