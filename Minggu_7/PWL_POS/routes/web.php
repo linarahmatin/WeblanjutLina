0a570eeecc351d4481ce0e86bea4865f04b1d7e1
linarahmatin <linarahmatin9@gmail.com>
2025-04-08 11:43:55 +0700

    JS7_Tugas 4 –Implementasi Form Registrasi

--- Minggu_7/PWL_POS/routes/web.php
+++ Minggu_7/PWL_POS/routes/web.php
@@ -163,7 +163,8 @@ use App\Http\Controllers\AuthController;
     Route::get('login', [AuthController::class, 'login'])->name('login');
     Route::post('login', [AuthController::class, 'postlogin']);
     Route::get('logout', [AuthController::class, 'logout'])->middleware('auth');
-
+    Route::get('register', [AuthController::class, 'register'])->name('register');
+    Route::post('register', [AuthController::class, 'postregister']);
     Route::middleware(['auth'])->group(function () { // artinya semua route di dalam group ini harus login dulu
 
     Route::get('/', [WelcomeController::class, 'index']);
