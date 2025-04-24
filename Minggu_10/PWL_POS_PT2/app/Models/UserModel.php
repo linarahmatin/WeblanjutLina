3ef2bc94a9dccfeaf181705581ecf91245264da5
linarahmatin <linarahmatin9@gmail.com>
2025-04-24 14:21:38 +0700

    JS10 Praktikum 1 – Membuat RESTful API Register

--- Minggu_10/PWL_POS_PT2/app/Models/UserModel.php
+++ Minggu_10/PWL_POS_PT2/app/Models/UserModel.php
@@ -4,12 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; //Implementasi class Authenticatable
 
 
 class UserModel extends Authenticatable
 {
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return[];
+    }
+    
     use HasFactory;
 
     protected $table = 'm_user'; //mendefinisikan nama tabel yang digunakan oleh model ini
