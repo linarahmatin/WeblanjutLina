acbdf8c0543b95581cba52fc832da073886b7cf5
linarahmatin <linarahmatin9@gmail.com>
2025-04-26 13:03:36 +0700

    JS11 Tugas-implementasi API tabel m_barang

--- Minggu_11/PWL_POS_PT2/app/Models/BarangModel.php
+++ Minggu_11/PWL_POS_PT2/app/Models/BarangModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class BarangModel extends Model
 {
@@ -11,18 +12,38 @@ class BarangModel extends Model
 
     protected $table = 'm_barang';
     protected $primaryKey = 'barang_id';
+    // protected $fillable = [
+    //     'barang_id',
+    //     'kategori_id',
+    //     'barang_kode',
+    //     'barang_nama',
+    //     'harga_beli',
+    //     'harga_jual',
+    // ];
+
+    // tugas jobsheet 11
     protected $fillable = [
-        'barang_id',
         'kategori_id',
         'barang_kode',
         'barang_nama',
         'harga_beli',
         'harga_jual',
+        'image',
+        'created_at',
+        'updated_at'
     ];
 
     public function kategori():BelongsTo
     {
         return $this->belongsTo(KategoriModel::class, 'kategori_id', 'kategori_id');
+    }
+
+    // tugas jobsheet 11
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/barang/' . $image),
+        );
     }
 }
   
